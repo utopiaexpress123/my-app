d2d33e2b026ebaed541e88d752ffb413c0d17898
rick-sys <ricksanchezy2x@gmail.com>
2024-03-20 15:18:01 +0100

    dfdf

--- components/ui/video.tsx
+++ components/ui/video.tsx
@@ -1,8 +1,8 @@
 "use client"
 export function Video() {
     return (
-      <video width="320" height="240" autoPlay loop muted playsInline preload="none">
-        <source src="/video.mp4" type="video/mp4" />
+      <video width="720" height="1408" autoPlay loop muted playsInline preload="none">
+        <source src="/video2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     )
