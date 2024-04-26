7860e810b9bf5277ecb889027745e9dd737b2435
rick-sys <ricksanchezy2x@gmail.com>
2024-04-26 20:28:32 +0200

    hjhjh

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { MainHero } from "@/components/component/main-hero";
-import { BentoCardsV2Video } from "@/components/component/bento-cards-v2-video";
-import { BentoCardsV4 } from "@/components/component/bento-cards-v4";
-import { BentoCardsV3 } from "@/components/component/bento-cards-v3";
 import { HowItWorks } from "@/components/component/how-it-works";
-import { Coupon } from "@/components/component/coupon";
-import { ThreeSteps } from "@/components/component/three-steps";
-import { Toolbar } from "@/components/component/lucy";
-import { PriceTable } from "@/components/component/price-table";
 import { Tamas } from "@/components/component/tamas";
 import { MainHeroV3 } from "@/components/component/main-hero-v3";
 import { Youtube } from "../components/component/youtube";
-import { Products } from "@/components/component/products";
 import { Lucy } from "@/components/component/lucy";
 import { Clone } from "@/components/component/clone";
 import { Contact } from "@/components/component/contact";
